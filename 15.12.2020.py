806fc23447e57b27e40d8cf53750d8526c5f7835
lenin445 <73684443+lenin445@users.noreply.github.com>
2021-01-03 12:40:43 +0530

    Update 15.12.2020.py

--- 15.12.2020.py
+++ 15.12.2020.py
@@ -1,105 +1,105 @@
 #str
-#print(str(10))
-#print(str(True))
-#print(str(False))
+print(str(10))
+print(str(True))
+print(str(False))
 
-#no = 100
-#name = "lenin"
-#tamilnadu = True
+no = 100
+name = "lenin"
+tamilnadu = True
 value = 10.21
-#print(name)
-#print(type(no))
-#print(type(name))
-#print(type(tamilnadu))
-#print(type(value))
-#print(id(no))
-#print(id(name))
-#print(id(tamilnadu))
-#print(id(value))
+print(name)
+print(type(no))
+print(type(name))
+print(type(tamilnadu))
+print(type(value))
+print(id(no))
+print(id(name))
+print(id(tamilnadu))
+print(id(value))
 
 
 state1 = "tamilnadu"
 state2 = "tamilnadu"
-#state1 = "kerala"
-#print(id(state1))
-#print(id(state2))
-#print(id(state1))
+state1 = "kerala"
+print(id(state1))
+print(id(state2))
+print(id(state1))
 
-#print("hi hello")
-#print('hi hello')
-##print("hi\thello")
-#print("hi\nhello")
-#print("hi\hello")
-#print("i'am 'lenin")
+print("hi hello")
+print('hi hello')
+print("hi\thello")
+print("hi\nhello")
+print("hi\hello")
+print("i'am 'lenin")
 
-##OPERATORS
-#arithmetic
-#print(5+5)
-#print(5-5)
-#print(2*3)
-#print(5/2)
-#print(5**2)
-#print(5//2)
-#print(5%2)
+OPERATORS
+arithmetic
+print(5+5)
+print(5-5)
+print(2*3)
+print(5/2)
+print(5**2)
+print(5//2)
+print(5%2)
 
-#Relational
-#print(10>5)
-#print(10<5)
-#print(100<200)
-#print(200<300)
-#print(100<=200)
+Relational
+print(10>5)
+print(10<5)
+print(100<200)
+print(200<300)
+print(100<=200)
 
-#in str 
-#name = "abcd"
-#name1 = "abcde"
-#print(name>name1)
+in str 
+name = "abcd"
+name1 = "abcde"
+print(name>name1)
 
-#chinging operators
-#print(10>9>8)
-#print(10<11>10)
+chinging operators
+print(10>9>8)
+print(10<11>10)
 
-#equality operators
+equality operators
 
-#print(10==20)
-#print(10!=20)
+print(10==20)
+print(10!=20)
 
-#print(10==100==200)
-#print(10!=100==100)
-#logical operators
-#print(5 and 0)
-#print(5 and 6)
-#print(5 or 0)
-#print(5 or 6)
+print(10==100==200)
+print(10!=100==100)
+logical operators
+print(5 and 0)
+print(5 and 6)
+print(5 or 0)
+print(5 or 6)
 
 sentence = ("lenin are u free now")
-#print("now" in sentence) 
+print("now" in sentence) 
 
-#TERNARY OPERATORS
+TERNARY OPERATORS
 
-#a = 30
-#b = 20 
-#c = 60 if a>b else 40
+a = 30
+b = 20 
+c = 60 if a>b else 40
 #print(c)
 
-#a = 700
-#b = 500
-#c = 300
-#min = a if a<b and a<c else b if b<c and b<a else c
-#print(min)
+a = 700
+b = 500
+c = 300
+min = a if a<b and a<c else b if b<c and b<a else c
+print(min)
 
-#a = 10
-#b = 20
-#print(a==b)
-#print(a is b)
+a = 10
+b = 20
+print(a==b)
+print(a is b)
 
-#name = input()
-#print("hi", name, "welcome to python")
+name = input()
+print("hi", name, "welcome to python")
 
-#name = input('enter ur name:')
-#print('hai',name,'u can have a seat')
+name = input('enter ur name:')
+print('hai',name,'u can have a seat')
 
-#mark = input("enter ur mark")
-#print(mark,'your mark is to low')
+mark = input("enter ur mark")
+print(mark,'your mark is to low')
 
 
 
